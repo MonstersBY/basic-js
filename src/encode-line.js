61f186ba7c1d196bf05bc3221c5e1cef81df8dfc
Alex Romanchenko <monsters.alex0@gmail.com>
2022-10-16 20:00:38 +0300

    feat: done tests

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  var arr = str.split('')
+  var count =1
+  var newStr = ''
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]==arr[i+1]){
+      count++
+    } else if (count == 1){
+      newStr +=arr[i]
+    } else {
+      newStr +=count
+      newStr +=arr[i]
+      count = 1
+    }
+  }
+  return newStr
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  var count =0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != -1){
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j]!= -1 && arr[i] < arr[j]){
+          count = arr[i]
+          arr[i] = arr[j]
+          arr[j] =count
+        }
+      }
+    }
+  }
+  return arr
 }
 
 module.exports = {
